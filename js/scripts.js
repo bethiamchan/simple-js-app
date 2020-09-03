befbee61d4412d88b247e484d89cbadc117c0e5a
Bethia Chan <bethiamchan@gmail.com>
2020-09-03 13:20:06 -0500

    Updated loop with if stmt indicating large Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 let pokemonList = [
-    {name: "Squirtle", height: 0.5, type: "water"},
-    {name: "Charmander", height: 0.6, type: "fire"},
-    {name: "Igglybuff", height: 0.3, type: ["fairy", "normal"]}
+    {
+        name: "Squirtle",
+        height: 0.5,
+        type: "water"
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        type: "fire"
+    },
+    {
+        name: "Igglybuff",
+        height: 0.3,
+        type: ["fairy", "normal"]
+    }
 ];
 
 for (let i = 0; i < pokemonList.length; i++){
-    document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>');
+    if (pokemonList[i].height > 0.5){
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!</p>');
+    } else {
+        document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') </p>');
+    }
 }
