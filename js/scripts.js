ccbb574b524fdb1b0f19e79241adc21c884bd808
Bethia Chan <bethiamchan@gmail.com>
2020-09-10 13:39:10 -0500

    Updated pokemonList to IIFE pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,37 @@
-let pokemonList = [
-    {
-        name: "Squirtle",
-        height: 0.5,
-        type: "water"
-    },
-    {
-        name: "Charmander",
-        height: 0.6,
-        type: "fire"
-    },
-    {
-        name: "Igglybuff",
-        height: 0.3,
-        type: ["fairy", "normal"]
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Squirtle",
+            height: 0.5,
+            type: "water"
+        },
+        {
+            name: "Charmander",
+            height: 0.6,
+            type: "fire"
+        },
+        {
+            name: "Igglybuff",
+            height: 0.3,
+            type: ["fairy", "normal"]
+        }
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList
+    };
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 0.5){
         document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!</p>');
     } else {
