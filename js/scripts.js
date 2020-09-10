edd3ce6eb97b5bf6020938f28428233a73947005
Bethia Chan <bethiamchan@gmail.com>
2020-09-10 14:23:28 -0500

    Check parameter type so only obj  added to array

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,12 @@ let pokemonRepository = (function () {
     ];
 
     function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
+        if (typeof(pokemon) === 'object') {
+            pokemonList.push(pokemon);
+          }else {
+            document.write("Please enter valid Pokemon details as an object.");
+          }
+    };
 
     function getAll() {
         return pokemonList
@@ -30,6 +34,12 @@ let pokemonRepository = (function () {
         getAll: getAll
     };
 })();
+
+// This correctly adds a pokemon to the pokemonList array, but it must come before the forEach function to write to doc correctly:
+// pokemonRepository.add({name: 'Pikachu'});
+
+// This is not added to the pokemonList array and prints error message to doc since argument is not an object:
+// pokemonRepository.add('pikachu');
 
 pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 0.5){
